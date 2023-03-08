59b03c7824bebb778860a7b87e3a1f050a386e42
Kevin Susanto <kevin_susanto@hotmail.com>
2023-03-08 16:54:29 +0700

    update footer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import BottomNavbar from './components/BottomNavbar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <header/>
+  <footer>
+    <BottomNavbar msg="n" />
+  </footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
+/*header {*/
+/*  line-height: 1.5;*/
+/*}*/
 
 .logo {
   display: block;
@@ -28,20 +22,20 @@ header {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+  /*header {*/
+  /*  display: flex;*/
+  /*  place-items: center;*/
+  /*  !*padding-right: calc(var(--section-gap) / 2);*!*/
+  /*}*/
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  /*.logo {*/
+  /*  margin: 0 2rem 0 0;*/
+  /*}*/
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  /*header .wrapper {*/
+  /*  display: flex;*/
+  /*  place-items: flex-start;*/
+  /*  flex-wrap: wrap;*/
+  /*}*/
 }
 </style>
---
+++ src/components/BottomNavbar.vue
@@ -0,0 +1,100 @@
+<script setup>
+defineProps({
+  home: {
+    type: String,
+    required: false
+  },
+  profile: {
+    type: String,
+    required: false
+  },
+  calendar: {
+    type: String,
+    required: false
+  },
+  photo: {
+    type: String,
+    required: false
+  },
+  gift: {
+    type: String,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <div class="footer">
+    <div class="imageChild">
+      <img alt="Vue logo" class="imageClass" src="@/assets/icon/wedding-invitation.png"/>
+    </div>
+    <div class="imageChild">
+      <img alt="Vue logo" class="imageClass" src="@/assets/icon/wedding-calendar.png"/>
+    </div>
+    <div class="imageChild">
+      <img alt="Vue logo" class="imageClass" src="@/assets/icon/wedding-profile.png"/>
+    </div>
+    <div class="imageChild">
+      <img alt="Vue logo" class="imageClass" src="@/assets/icon/wedding-camera.png"/>
+    </div>
+    <div class="imageChild">
+      <img alt="Vue logo" class="imageClass" src="@/assets/icon/wedding-invitation.png"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  max-width: 90vw;
+  background-color: lightgrey;
+  margin: auto;
+  border-radius: 20px 20px 0 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.footer div{
+  margin: 10px;
+}
+
+.imageChild{
+  min-width: 25px;
+  min-height: 25px;
+}
+
+.imageClass{
+  width: 25px;
+  height: 25px;
+}
+
+@media (min-width: 1024px) {
+  .footer {
+    max-width: 70vw;
+    background-color: lightgrey;
+    margin: auto;
+    border-radius: 20px 20px 0 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+
+  .imageChild{
+    min-width: 50px;
+    min-height: 50px;
+  }
+
+  .imageClass{
+    width: 50px;
+    height: 50px;
+  }
+}
+</style>
